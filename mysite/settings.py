c5eab7062be7b9aa96f3f3e0136b38922fbb9aaf
przeuas <przeus13@o2.pl>
2018-09-15 20:26:32 +0200

    z sql lite

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,15 +74,21 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'python',
+#         'USER': 'postgres',
+#         'PASSWORD': 'xere#x',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'python',
-        'USER': 'postgres',
-        'PASSWORD': 'xere#x',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
 }
 
 
